9a9c1b50274f7d5d5b1c573ef703a12f6a414c9d
senhor cinema <josethiagosantos0802@gmail.com>
2025-05-25 09:51:41 -0300

    Add files via upload

---
+++ Cons/config/banco.sql
@@ -0,0 +1,13 @@
+create database Cons;
+use Cons;
+
+create table Console(
+id int auto_increment primary key,
+ano varchar(250),
+est varchar(250),
+pre decimal(16,2),
+marc varchar(250),
+mode varchar(250),
+anexo varchar(250));
+
+select * from Console;
